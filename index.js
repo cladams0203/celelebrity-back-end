aa9326d22760c111c0be0ebe309098d574f88ce7
cladams0203 <cladams0203@gmail.com>
2020-04-12 13:02:58 -0400

    added welcome message

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 const server = require('./server')
 
 const PORT = process.env.PORT || 5000
-
+server.get('/', (req,res) => {
+    res.status(200).json({message:  'serveris running'})
+})
 server.listen(PORT, () => {
     console.log(`server is running on ${PORT}`)
 })
